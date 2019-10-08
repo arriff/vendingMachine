4612a4b2b101c1b612253d30aa18f8f76fef3d53
arriff <m-adda@hotmail.nl>
2019-10-08 16:42:14 +0200

    commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var cola = "1,50";
+var fanta = "1,70";
+var sprite = "2,50";
+
+function buttonPressed(numberPressed){
+console.log("bla");
+document.getElementById("display_text_cost").innerHTML = numberPressed;
+}
+function productName(pname){
+document.getElementById("display_text_product").innerHTML = pname;
+}
